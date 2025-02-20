765ff89abc638b76926669ef985ec194ecdd92bc
Vishwas JS <vishwasjs2806@gmail.com>
2025-02-20 12:13:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function toggleFunction() {
+   
+    var x = document.getElementById("topnav");
+    x.classList.toggle("expanded");
+}
+
+function checkOrientation() {
+    var x = document.getElementById("topnav");
+    if (window.matchMedia("(orientation: landscape)").matches) { // Change the class name to "landscape" when in landscape orientation
+
+        if (x.className === "nav-container responsive") {
+            x.className = "nav-container"
+        }
+
+
+    } else {}
+}
